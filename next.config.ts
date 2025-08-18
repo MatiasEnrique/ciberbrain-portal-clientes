cd6f6c973f617137ae72a0df0a58738a69625757
Matias Enrique <mati.seba.enrique@gmail.com>
2025-08-17 21:59:15 -0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
-  images: {
-    domains: ["ciberbrain.net"],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb",
-    },
-  },
-} satisfies NextConfig;
+const nextConfig = {} satisfies NextConfig;
 
 module.exports = nextConfig;
